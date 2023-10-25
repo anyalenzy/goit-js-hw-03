052413469ffbe925bbf2ec3d870451d5cb495e55
anyalenzy <anya.dudnik.91@gmail.com>
2023-10-25 19:20:34 +0300

    done task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,15 @@
 "use strict";
+function filterArray(numbers, value) {
+  const newArray = [];
+  for (const i of numbers) {
+    if (i > value) {
+      newArray.push(i);
+    }
+  }
+  return newArray;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
