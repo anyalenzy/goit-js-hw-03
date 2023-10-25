f48b6ebd9a207dadc100542ddb2a7a36d128cf13
anyalenzy <anya.dudnik.91@gmail.com>
2023-10-25 12:33:24 +0300

    done task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,11 @@
 "use strict";
+function slugify(title) {
+  const titleLower = title.toLowerCase();
+  const titleArrive = titleLower.split(` `);
+  const slag = titleArrive.join(`-`);
+  return slag;
+}
+console.log(slugify("Arrays for begginers"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
